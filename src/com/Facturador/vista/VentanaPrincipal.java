7d4bf18d5cefac81e40982bba4b73ca93bc66c88
Jorge Goncalves <leomarcian19@cock.li>
2016-01-16 12:55:51 -0430

    Conflictos arreglados

--- src/com/Facturador/vista/VentanaPrincipal.java
+++ src/com/Facturador/vista/VentanaPrincipal.java
@@ -8,6 +8,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
 
 public class VentanaPrincipal extends JFrame { //Se crea la clase ventana principal
 	private JPanel contentPane;
@@ -15,6 +18,15 @@ public class VentanaPrincipal extends JFrame { //Se crea la clase ventana princi
 		setTitle("Facturador A.R.R.E.C.H.O"); //Se crea el objeto
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1024, 768);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Algo");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Otra cosa");
+		mnNewMenu.add(mntmNewMenuItem);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
