7f57ef2dec3fc3abe573f134259e892edeb2b522
Jorge Goncalves <leomarcian19@cock.li>
2016-01-17 21:34:57 -0430

    Arreglada verificación de campos vacios en Control Cliente

--- src/com/Facturador/controlador/ControlRegistrarCliente.java
+++ src/com/Facturador/controlador/ControlRegistrarCliente.java
@@ -3,6 +3,8 @@ package com.Facturador.controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -44,34 +46,40 @@ public class ControlRegistrarCliente implements ActionListener  {
 				
 			}	
 			if(this.cha.equals("GuardCliente")){
-				System.out.println("Comenzó el parseo");
-				String temp = q.clien.textClienCedu.getText();
-				int temp_cedu = Integer.parseInt(temp);
-				String temp_nombre = q.clien.textClienNombre.getText();
-				String temp_telf = q.clien.textClienTelf.getText();
-				Integer temp_tel = Integer.parseInt(temp_telf);
-				String temp_dir = q.clien.textClienDirec.getText();//Parseo de todas las variables a objeto, si conoces una forma mas rapida de hacerla implementala
-				System.out.println("Lo parseo todo, comienza la vaina");
 				
-				Cliente cliente = new Cliente(temp_cedu, temp_nombre, temp_tel, temp_dir); //Instancia al nuevo cliente, con su cedula, nombre, telefono y dirección
-				System.out.println("Instanceo");
-				
-				Session session = Mysql.getSession();  //Crea el objeto Session usando la funcion getSEssion de la clase Mysql
-				System.out.println("Creo la sesion");
-				
-				Transaction tx = session.beginTransaction(); //Crea o comienza la transacción, similar a git init
-				System.out.println("Creo la transacción");
-				
-				session.save(cliente); //Salva a cliente, similar a git add .
-				System.out.println("Salvo a cliente");
-				
-				tx.commit(); // Hace commit a la nueva entrada, similar a git commit -m "xyz"
-				System.out.println("Hizo commit en la transaccion");
-				
-				session.close(); //Cierra la sesión (Abre la puerta y luego la cierra)
-				System.out.println("Cerro la sesion y todo terminó bien");
-				//Se agregaron los System.out.println(arg0); a medida de comprobar que las cosas fuesen sucediendo, se removeran mas adelante
-				//Eventualmente la idea es hacer esto con Try y Catch
+				if (q.clien.textClienApellido.getText()==null||q.clien.textClienCedu.getText()==null||q.clien.textClienDirec.getText()==null||q.clien.textClienNombre.getText()==null||q.clien.textClienTelf.getText()==null){
+					System.out.println("Comenzó el parseo");
+					String temp = q.clien.textClienCedu.getText();
+					int temp_cedu = Integer.parseInt(temp);
+					String temp_nombre = q.clien.textClienNombre.getText();
+					String temp_telf = q.clien.textClienTelf.getText();
+					Integer temp_tel = Integer.parseInt(temp_telf);
+					String temp_dir = q.clien.textClienDirec.getText();//Parseo de todas las variables a objeto, si conoces una forma mas rapida de hacerla implementala
+					System.out.println("Lo parseo todo, comienza la vaina");
+					
+					Cliente cliente = new Cliente(temp_cedu, temp_nombre, temp_tel, temp_dir); //Instancia al nuevo cliente, con su cedula, nombre, telefono y dirección
+					System.out.println("Instanceo");
+					
+					Session session = Mysql.getSession();  //Crea el objeto Session usando la funcion getSEssion de la clase Mysql
+					System.out.println("Creo la sesion");
+					
+					Transaction tx = session.beginTransaction(); //Crea o comienza la transacción, similar a git init
+					System.out.println("Creo la transacción");
+					
+					session.save(cliente); //Salva a cliente, similar a git add .
+					System.out.println("Salvo a cliente");
+					
+					tx.commit(); // Hace commit a la nueva entrada, similar a git commit -m "xyz"
+					System.out.println("Hizo commit en la transaccion");
+					
+					session.close(); //Cierra la sesión (Abre la puerta y luego la cierra)
+					System.out.println("Cerro la sesion y todo terminó bien");
+					//Se agregaron los System.out.println(arg0); a medida de comprobar que las cosas fuesen sucediendo, se removeran mas adelante
+					//Eventualmente la idea es hacer esto con Try y Catch
+				}
+				else{
+					JOptionPane.showMessageDialog(escu, "No puedes dejar espacios vacios", "Error", JOptionPane.ERROR_MESSAGE); //Agregada verificación
+				}
 			}	
 			
 			
