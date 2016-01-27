06150373d95470e5edfa58a2112bf5635044a106
Jorge Goncalves <leomarcian19@cock.li>
2016-01-27 02:18:29 -0430

    Cambios RADICALES en el Login.

--- src/com/Facturador/controlador/ControlLogin.java
+++ src/com/Facturador/controlador/ControlLogin.java
@@ -2,15 +2,51 @@ package com.Facturador.controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JOptionPane;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import com.Facturador.modelo.Mysql;
 import com.Facturador.vista.Login;
-import com.Facturador.vista.RegistrarCliente;
 
 public class ControlLogin implements ActionListener{
 	private  Login escu; 
 	private String cha;
 	
-	
+	ClasePrincipal q = new ClasePrincipal();
+	public void Login(){
+		JOptionPane.showMessageDialog(escu, "Si es tu primera vez usando el programa, debes saber que las cuentas estan guardadas en la entidad 'Encargado' de la BD");
+		//En otras palabras, registra un usuario/contraseña de forma manual por PHPMyAdmin
+		if(q.Log.textUser.getText().equals("")||q.Log.textPass.getText().equals("")){
+			JOptionPane.showMessageDialog(escu, "Tienes campos vacios", "Imposible iniciar sesión", JOptionPane.ERROR_MESSAGE);
+		}else{
+			Session session = Mysql.getSession();
+			String hql = "FROM Encargado E WHERE E.username = :Usr";
+			Query query = session.createQuery(hql);
+			query.setParameter("Usr", q.Log.textUser.getText());
+			List results = query.list();
+			System.out.println(results.size());
+			if(results.size()==1){
+				String pass = q.Log.textPass.getText();
+				hql = "FROM Encargado E WHERE E.pass = :Pass";
+				query = session.createQuery(hql);
+				query.setParameter("Pass", q.Log.textPass.getText());
+				results = query.list();
+				System.out.println(results.size());
+				if (results.size()==1){
+					JOptionPane.showMessageDialog(escu, "Bienvenido "+q.Log.textUser.getText(), "Datos correctos", JOptionPane.DEFAULT_OPTION);
+					q.Log.setVisible(false);
+					q.VentPrin.setVisible(true);
+				}else{
+					JOptionPane.showMessageDialog(escu, "Verifique su contraseña e intente de nuevo", "Datos incorrectos", JOptionPane.ERROR_MESSAGE);
+				}
+			}else{
+				JOptionPane.showMessageDialog(escu, "No existe ese usuario en el registro", "Datos incorrectos", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
 	    public ControlLogin(Login escu, String cha) { 
 	    																	
 	        this.escu = escu; 
@@ -22,21 +58,10 @@ public class ControlLogin implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			
-			ClasePrincipal q = new ClasePrincipal();
+			
 			
 			if(this.cha.equals("Buscar")){
-				
-				if(q.Log.textPass.getText().equals("admin") & q.Log.textUser.getText().equals("admin")){
-					q.Log.setVisible(false);
-					q.VentPrin.setVisible(true);
-					
-				}
-				else{
-					System.out.println("usuario y contraseña incorrecto");
-					System.out.println("usuario= admin");
-					System.out.println("contraseña= admin");
-				}
-				
+				Login();
 			}	
 		}
 	    
--- src/com/Facturador/vista/Login.java
+++ src/com/Facturador/vista/Login.java
@@ -16,12 +16,16 @@ import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import javax.swing.JPasswordField;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
 	public static JTextField textUser;
 	public static JTextField textPass;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -43,39 +47,56 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/com/Facturador/images/login7.png")));
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 272, 222);
+		setBounds(100, 100, 565, 390);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 153, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel Head = new JLabel("");
+		Head.setIcon(new ImageIcon(Login.class.getResource("/com/Facturador/images/Login.png")));
+		Head.setBounds(122, 24, 348, 94);
+		contentPane.add(Head);
+		
 		JLabel lblNewLabel = new JLabel("Password");
-		lblNewLabel.setFont(new Font("Stencil", Font.PLAIN, 14));
-		lblNewLabel.setBounds(26, 41, 86, 14);
+		lblNewLabel.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel.setFont(new Font("Stencil", Font.PLAIN, 22));
+		lblNewLabel.setBounds(142, 185, 116, 28);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("UserName");
-		lblNewLabel_1.setFont(new Font("Stencil", Font.PLAIN, 14));
-		lblNewLabel_1.setBounds(26, 88, 130, 14);
+		lblNewLabel_1.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_1.setFont(new Font("Stencil", Font.PLAIN, 22));
+		lblNewLabel_1.setBounds(142, 142, 116, 27);
 		contentPane.add(lblNewLabel_1);
 		
-		textPass = new JTextField();
-		textPass.setBounds(126, 38, 101, 20);
+		textPass = new JPasswordField();
+		textPass.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textPass.setBounds(309, 185, 124, 31);
 		contentPane.add(textPass);
 		textPass.setColumns(10);
 		
 		textUser = new JTextField();
-		textUser.setBounds(126, 85, 101, 20);
+		textUser.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textUser.setBounds(309, 141, 124, 31);
 		contentPane.add(textUser);
 		textUser.setColumns(10);
 		
 		JButton btnBuss = new JButton("Buscar");
 		btnBuss.addActionListener(new ControlLogin(this,"Buscar"));
-		btnBuss.setBounds(71, 129, 110, 44);
+		btnBuss.setBounds(212, 272, 110, 44);
 		contentPane.add(btnBuss);
+		
+		JLabel background = new JLabel("");
+		background.setIcon(new ImageIcon(Login.class.getResource("/com/Facturador/images/cbdef475a08645847520cd2576387880.jpg")));
+		background.setBounds(0, 0, 559, 361);
+		contentPane.add(background);
+		
+		
 	}
-
 }
